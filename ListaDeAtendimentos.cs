8aa366054cf3756dd4daacd0f7d87a8f4e2ffb11
joao ramos <jvitorsb98@outlook.com>
2023-12-01 21:08:20 -0300

    Sistema Medico

---
+++ ListaDeAtendimentos.cs
@@ -0,0 +1,26 @@
+namespace SistemaMedico
+{
+    public class ListaDeAtendimentos
+    {
+        private List<Atendimento> listaAtendimentos;
+
+        public ListaDeAtendimentos()
+        {
+            listaAtendimentos = new List<Atendimento>();
+        }
+
+        public List<Atendimento> ListaAtendimentos{
+            get{ return listaAtendimentos;}
+        }
+
+        public void AdicionarAtendimento(Atendimento novoAtendimento)
+        {
+            listaAtendimentos.Add(novoAtendimento);
+        }
+
+        public static Atendimento? ObterAtendimentoAtivo(ListaDeAtendimentos atendimentos, string cpfPaciente, string crmMedico)
+        {
+            return atendimentos.listaAtendimentos.FirstOrDefault(p => p.MedicoResponsavel.CRM == crmMedico && p.PacienteAtendido.CPF == cpfPaciente && p.AtendimentoEmAberto == true);
+        }
+    }
+}
