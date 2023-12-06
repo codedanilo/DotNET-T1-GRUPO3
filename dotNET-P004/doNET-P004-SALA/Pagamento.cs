8ae6c084f0f1aee28537adf6b502b8b89b322bf8
Eduardo S. Rocha <133709733+eduardsroch@users.noreply.github.com>
2023-12-06 09:57:19 -0300

    Criando a classe pagamento
    Classe pagamento

---
+++ dotNET-P004/doNET-P004-SALA/Pagamento.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public enum TipoPagamento
+{
+    CartaoCredito,
+    BoletoBancario,
+    Dinheiro
+}
+
+public class Pagamento
+{
+    public TipoPagamento Tipo { get; set; }
+    public string Descricao { get; set; }
+    public double ValorBruto { get; set; }
+    public double Desconto { get; set; }
+    public DateTime DataHora { get; set; }
+
+    public double CalcularValorFinal()
+    {
+    
+        return ValorBruto - Desconto;
+    }
+
+    public void RealizarPagamento()
+    {
+        switch (Tipo)
+        {
+            case TipoPagamento.CartaoCredito:
+                Console.WriteLine($"Pagamento realizado com cartão de crédito no valor de {CalcularValorFinal()}.");
+                break;
+            case TipoPagamento.BoletoBancario:
+                Console.WriteLine($"Emitindo boleto bancário para o pagamento no valor de {CalcularValorFinal()}.");
+                break;
+            case TipoPagamento.Dinheiro:
+                Console.WriteLine($"Pagamento em dinheiro recebido no valor de {CalcularValorFinal()}.");
+                break;
+            default:
+                Console.WriteLine("Tipo de pagamento inválido.");
+                break;
+        }
+    }
+}
