3b3c5356060163417ca53f4e944b3524d20409a2
joao ramos <jvitorsb98@outlook.com>
2023-12-06 09:52:25 -0300

    Adicionando ListaDePlanosDeSaude

--- PI-004/Cadastros.cs
+++ PI-004/Cadastros.cs
@@ -42,59 +42,91 @@ namespace SistemaMedico
             Console.ReadKey();
         }
 
-    public static void CadastrarPaciente(ListaDePacientes pacientes)
-    {
-        Console.Clear();
-
-        Console.WriteLine("---- Cadastro de Pacientes ----");
-
-        Console.Write("Nome: ");
-        string nome = Console.ReadLine() ?? "";
-
-        Console.Write("Data de Nascimento (DD/MM/AAAA): ");
-        if (DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dataNascimento))
+        public static void CadastrarPaciente(ListaDePacientes pacientes)
         {
-            Console.Write("CPF: ");
-            string? cpf = Console.ReadLine() ?? "";
+            Console.Clear();
 
-            Console.Write("Sexo (opcional - pressione Enter para pular): ");
-            string? sexo = Console.ReadLine();
-            
-            try
-            {   
-                Paciente novoPaciente = string.IsNullOrWhiteSpace(sexo)
-                    ? new Paciente(nome, dataNascimento, cpf)
-                    : new Paciente(nome, dataNascimento, cpf, sexo);
+            Console.WriteLine("---- Cadastro de Pacientes ----");
 
-                Console.WriteLine("Adicione os sintomas do paciente. Digite '0' para parar.");
-                do
-                {
-                    Console.Write("Sintoma: ");
-                    string? sintoma = Console.ReadLine() ?? "";
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine() ?? "";
 
-                    if (sintoma.ToLower() == "0")
-                        break;
-
-                    novoPaciente.AdicionarSintoma(sintoma);
-
-                    Console.WriteLine("Sintoma adicionado com sucesso!");
-                } while (true);
-                pacientes.AdicionarPaciente(novoPaciente);
-                Console.WriteLine("Paciente cadastrado com sucesso!");
-            }
-            catch (Exception ex)
+            Console.Write("Data de Nascimento (DD/MM/AAAA): ");
+            if (DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dataNascimento))
             {
-                Console.WriteLine($"Erro ao cadastrar paciente: {ex.Message}");
+                Console.Write("CPF: ");
+                string? cpf = Console.ReadLine() ?? "";
+
+                Console.Write("Sexo (opcional - pressione Enter para pular): ");
+                string? sexo = Console.ReadLine();
+                
+                try
+                {   
+                    Paciente novoPaciente = string.IsNullOrWhiteSpace(sexo)
+                        ? new Paciente(nome, dataNascimento, cpf)
+                        : new Paciente(nome, dataNascimento, cpf, sexo);
+
+                    Console.WriteLine("Adicione os sintomas do paciente. Digite '0' para parar.");
+                    do
+                    {
+                        Console.Write("Sintoma: ");
+                        string? sintoma = Console.ReadLine() ?? "";
+
+                        if (sintoma.ToLower() == "0")
+                            break;
+
+                        novoPaciente.AdicionarSintoma(sintoma);
+
+                        Console.WriteLine("Sintoma adicionado com sucesso!");
+                    } while (true);
+                    pacientes.AdicionarPaciente(novoPaciente);
+                    Console.WriteLine("Paciente cadastrado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao cadastrar paciente: {ex.Message}");
+                }
             }
-        }
-        else
-        {
-            Console.WriteLine("Formato de data inválido. Tente novamente.");
+            else
+            {
+                Console.WriteLine("Formato de data inválido. Tente novamente.");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para voltar ao Menu Principal...");
+            Console.ReadKey();
         }
 
-        Console.WriteLine("Pressione qualquer tecla para voltar ao Menu Principal...");
-        Console.ReadKey();
-    }
+        public static void CadastrarPlanoDeSaude(ListaDePlanosDeSaude planosDeSaude)
+        {
+            Console.Clear();
+
+            Console.WriteLine("---- Cadastro de Plano de Saúde ----");
+
+            Console.Write("Título: ");
+            string titulo = Console.ReadLine() ?? "";
+
+            Console.Write("Valor por Mês: ");
+            if (double.TryParse(Console.ReadLine(), NumberStyles.Currency, CultureInfo.CurrentCulture, out double valorPorMes))
+            {
+                try
+                {
+                    PlanoDeSaude novoPlano = new PlanoDeSaude(titulo, valorPorMes);
+                    planosDeSaude.AdicionarPlanoDeSaude(novoPlano);
+                    Console.WriteLine("Plano de Saúde cadastrado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao cadastrar Plano de Saúde: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Formato de valor inválido. Tente novamente.");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para voltar ao Menu Principal...");
+            Console.ReadKey();
+        }
 
     }
 
