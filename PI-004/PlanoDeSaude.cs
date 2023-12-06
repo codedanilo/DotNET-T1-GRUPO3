d2d53cd5ec5e3f47c5f5bf490c545c751e590fa2
joao ramos <jvitorsb98@outlook.com>
2023-12-06 09:44:55 -0300

    Plano de saude feito por daniel Monteiro

---
+++ PI-004/PlanoDeSaude.cs
@@ -0,0 +1,12 @@
+public class PlanoDeSaude
+{
+    public string Titulo { get; set; }
+
+    public double ValorPorMes { get; set; }
+
+    public PlanoDeSaude(string titulo, double valorPorMes)
+    {
+        Titulo = titulo;
+        ValorPorMes = valorPorMes;
+    }
+}
